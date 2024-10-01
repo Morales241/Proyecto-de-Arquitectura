11b76da2b473a3f6f8452624bb82b62bee62857f
Nube28 <159978169+Nube28@users.noreply.github.com>
2024-09-30 23:18:35 -0700

    quit unused imports
    amosay en ingles TRAAAA

--- DomoDominoMvc/src/main/java/loginMvc/LoginView.java
+++ DomoDominoMvc/src/main/java/loginMvc/LoginView.java
@@ -1,8 +1,6 @@
 package loginMvc;
 
-import javax.swing.JButton;
 import loginMvc.LoginModel;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class LoginView extends javax.swing.JFrame {
