b4fbde9f2e1aad9716e100e76ddfdbbcecb34b59
Jesus Morales <jesus.morales245335@potros.itson.edu.mx>
2024-10-02 04:24:48 -0700

    avance de comandos

--- DomoDominoMvc/src/main/java/crearMesa/CrearMesaController.java
+++ DomoDominoMvc/src/main/java/crearMesa/CrearMesaController.java
@@ -4,11 +4,35 @@
  */
 package crearMesa;
 
+import comands.IComando;
+import observers.IObserver;
+
 /**
  *
  * @author tacot
  */
-public class CrearMesaController {
+public class CrearMesaController implements IObserver{
     private CrearMesaView vista;
     private CrearMesaModel modelo;
+
+    public CrearMesaController(CrearMesaView vista, CrearMesaModel modelo) {
+        this.vista = vista;
+        this.modelo = modelo;
+    }
+
+    @Override
+    public void actualizar(IComando comando) {
+        if (comando != null) {
+            comando.ejecutarComando();
+        }
+    }
+    
+    public void Regresar(){
+    //codigo pa regresar asajssjad
+    }
+    
+    public void CrearMesa(){
+    //codigo pa crear mesa jajsaj
+    }
+    
 }
