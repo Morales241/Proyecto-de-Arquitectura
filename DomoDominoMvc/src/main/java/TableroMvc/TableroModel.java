fb4c36ba53f8912c677c7204d2ebc94d0f16de10
Nube28 <159978169+Nube28@users.noreply.github.com>
2024-10-02 05:46:59 -0700

    Tablero corregido
    ola
    
    Co-Authored-By: Jesus Morales <163379761+Morales241@users.noreply.github.com>
    Co-Authored-By: RobertoFavela <160526441+RobertoFavela@users.noreply.github.com>

--- DomoDominoMvc/src/main/java/TableroMvc/TableroModel.java
+++ DomoDominoMvc/src/main/java/TableroMvc/TableroModel.java
@@ -1,13 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package TableroMvc;
 
-/**
- *
- * @author favel
- */
-public class TableroModel {
+import java.util.ArrayList;
+import java.util.List;
+import observers.IObservable;
+import observers.IObserver;
+import pozo.Pozo;
+
+public class TableroModel implements IObservable {
+    
+    private Pozo pozo;
+    private int numeroFichas;
+    private List<IObserver> observadores = new ArrayList<>();
+    
+    public void repartirFichas(){
+        pozo = new Pozo(numeroFichas);
+        pozo.repartirFichas();
+    }
+    
+    @Override
+    public void agregarObservador(IObserver observador) {
+        observadores.add(observador);
+    }
+
+    @Override
+    public void eliminarObservador(IObserver observador) {
+        observadores.remove(observador);
+    }
+
+    @Override
+    public void notificarObservadores(String mensaje) {
+        observadores.forEach(IObserver -> {
+            IObserver.actualizar(mensaje);
+        });
+    }
+    
     
 }
