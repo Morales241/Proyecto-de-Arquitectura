4a35866b1c8381e001ac06b34002c7805b7c0c07
RobertoFavela <160526441+RobertoFavela@users.noreply.github.com>
2024-10-09 14:05:44 -0600

    Comentarios

--- DomoDominoDominio/src/main/java/Entidades/Arreglo.java
+++ DomoDominoDominio/src/main/java/Entidades/Arreglo.java
@@ -10,8 +10,8 @@ public class Arreglo {
 
     private int[][] tablero;
 
-    public Arreglo() {
-        tablero = new int[17][30];
+    public Arreglo(int ancho, int alto) {
+        tablero = new int[ancho][alto];
         reiniciarTablero();
     }
 
@@ -19,37 +19,39 @@ public class Arreglo {
         return tablero[fila][columna] == 0;
     }
 
-    public boolean colocarFichaHorizontal(Ficha ficha, int fila, int columna) {
-        if (ficha.esMula()) {
-            if (estaVacio(fila, columna)) {
-                tablero[fila][columna] = ficha.getLado1();
-                return true;
-            }
-        } else {
-            if (estaVacio(fila, columna) && estaVacio(fila, columna + 1)) {
-                tablero[fila][columna] = ficha.getLado1();
-                tablero[fila][columna + 1] = ficha.getLado2();
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean colocarFichaVertical(Ficha ficha, int fila, int columna) {
-        if (ficha.esMula()) {
-            if (estaVacio(fila, columna)) {
-                tablero[fila][columna] = ficha.getLado1();
-                return true;
-            }
-        } else {
-            if (estaVacio(fila, columna) && estaVacio(fila, columna + 1)) {
-                tablero[fila][columna] = ficha.getLado1();
-                tablero[fila + 1][columna] = ficha.getLado2();
-                return true;
-            }
-        }
-        return false;
-    }
+//    //Esto no va aqui
+//    public boolean colocarFichaHorizontal(Ficha ficha, int fila, int columna) {
+//        if (ficha.esMula()) {
+//            if (estaVacio(fila, columna)) {
+//                tablero[fila][columna] = ficha.getLado1();
+//                return true;
+//            }
+//        } else {
+//            if (estaVacio(fila, columna) && estaVacio(fila, columna + 1)) {
+//                tablero[fila][columna] = ficha.getLado1();
+//                tablero[fila][columna + 1] = ficha.getLado2();
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//
+//    //Este tampoco va aqui
+//    public boolean colocarFichaVertical(Ficha ficha, int fila, int columna) {
+//        if (ficha.esMula()) {
+//            if (estaVacio(fila, columna)) {
+//                tablero[fila][columna] = ficha.getLado1();
+//                return true;
+//            }
+//        } else {
+//            if (estaVacio(fila, columna) && estaVacio(fila, columna + 1)) {
+//                tablero[fila][columna] = ficha.getLado1();
+//                tablero[fila + 1][columna] = ficha.getLado2();
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 
     public int obtenerFicha(int fila, int columna) {
         return tablero[fila][columna];
