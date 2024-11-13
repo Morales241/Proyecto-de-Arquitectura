09cd85792c8c7ff5d1eed1bfcabd96bbe011a66b
Jesus Morales <jesus.morales245335@potros.itson.edu.mx>
2024-11-12 23:18:47 -0700

    Reapply "Merge branch 'Jesus'"
    This reverts commit c24c75a4b959fb772274e1b80f5747202681e60d.

--- DomoDominoServerCentral/src/main/java/sereverCentral/ArrancarServidorCentral.java
+++ DomoDominoServerCentral/src/main/java/sereverCentral/ArrancarServidorCentral.java
@@ -44,7 +44,6 @@ public class ArrancarServidorCentral {
             System.out.println(llave);
         }
         System.out.println("va a mandar algo");
-        cliente.enviarMensaje(new JugadorCrearPartidaDto("2", "3", 1));
 
     }
 
