97b56363c3ac15175c7d097fe8b7a187524ad6a3
Jesus Morales <jesus.morales245335@potros.itson.edu.mx>
2024-11-20 01:06:47 -0700

    creacion de clases

--- DomoDominoServerCentralServidor/src/main/java/servidor/Servidor.java
+++ DomoDominoServerCentralServidor/src/main/java/servidor/Servidor.java
@@ -81,6 +81,7 @@ public class Servidor {
                     case JugadorAEliminarDto jugadorAEliminarDto ->
                         gestorMensajes.notificarObserverSalirDePartida(jugadorAEliminarDto);
 
+                        
                     default ->
                         log.log(Level.INFO, "Tipo de mensaje no reconocido");
                 }
